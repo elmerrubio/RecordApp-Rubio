15a2af0028173a422139f54c48c6644d6f6e4b78
elmerrubio <elmerrubiojr25@gmail.com>
2022-04-05 13:37:18 +0800

    config are done

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/Bautista_RecordApp/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'dumbpotato');
+	define('DB_NAME', 'record_app');
